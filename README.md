7e9a5e58e7238672672187b842bb2ee075062478
SpaceGhost <lukasz.celitan@gmail.com>
2019-03-03 18:50:00 +0100

    added page request option to the api call

---
+++ README.md
@@ -0,0 +1,55 @@
+# gatsby-source-woocommerce
+
+Source plugin for [Gatsby](https://www.gatsbyjs.org/). Pulls in data from protected routes via the [WooCommerce REST API](http://woocommerce.github.io/woocommerce-rest-api-docs/) with credentials.
+
+## Install
+
+`npm install --save gatsby-source-woocommerce`
+
+## How to Use
+
+```javascript
+// In gatsby-config.js
+plugins:[
+  {       
+    resolve: "gatsby-source-woocommerce",
+    options: {
+     // Base URL of Wordpress site
+     
+      // This counts controls the API get with ?per_page=
+      // type number
+      itemCount: 20,
+
+      api: 'wordpress.domain',
+      // true if using https. false if nah.
+      https: false,
+      api_keys: {
+        consumer_key: <key>,
+        consumer_secret: <secret>,
+      },
+      // Array of strings with fields you'd like to create nodes for...
+      fields: ['products']
+    }
+  }
+]
+```
+
+## Currently Supported Fields
+
+- Products
+- Customers
+- Orders
+- Reports
+- Coupons
+
+
+### FORK
+
+This is fork from https://registry.npmjs.org/gatsby-source-woocommerce/ 
+
+### @SPACEGHOST
+
+03.03. 2019
+Added support for page pagination via ?per_page= request for displaying more than 10 products in a single call. 
+
+
---
+++ gatsby-node.js
@@ -0,0 +1,52 @@
+const WooCommerceAPI = require('woocommerce-api');
+const { processNode } = require('./helpers');
+
+exports.sourceNodes = async (
+  { boundActionCreators, createNodeId },
+  configOptions
+) => {
+  const { createNode } = boundActionCreators;
+  delete configOptions.plugins;
+
+  const { api, https, api_keys, fields, itemCount } = configOptions;
+
+  // set up WooCommerce node api tool
+  const WooCommerce = new WooCommerceAPI({
+    url: `http${https?'s':''}://${api}`,
+    consumerKey: api_keys.consumer_key,
+    consumerSecret: api_keys.consumer_secret,
+    wpAPI: true,
+    version: 'wc/v1',
+  });
+
+ 
+
+  // Fetch Node and turn our response to JSON
+  const fetchNodes = async (fieldName) => {
+
+    let pag = `${fieldName}?per_page=${itemCount}`
+    if (itemCount === null || typeof itemCount !== 'undefined') {
+      pag = fieldName
+    }
+    const res = await WooCommerce.getAsync(pag);
+    return JSON.parse(res.toJSON().body);
+  };
+
+  // Loop over each field set in configOptions and process/create nodes
+  async function fetchNodesAndCreate (array) {
+    for (const field of array) {
+      const nodes = await fetchNodes(field);
+      nodes.forEach(n=>createNode(processNode(createNodeId, n, field)));
+    }
+  }
+  
+
+
+
+
+
+
+  // Leh go...
+  await fetchNodesAndCreate(fields);
+  return;
+};
